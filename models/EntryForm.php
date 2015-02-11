c2f6bdfde0c4b940a5e304fc2203bf575b7fadd6
123wly <123wly@sina.cn>
2015-02-11 11:18:24 +0800

    111

---
+++ models/EntryForm.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\models;
+use yii\base\model;
+class EntryForm extends model
+{
+    public $name;
+    public $email;
+
+    public function rules()
+    {
+    return[
+        [['name','email'],'required'],
+        ['email','email']
+    ];
+    }
+
+}
+
